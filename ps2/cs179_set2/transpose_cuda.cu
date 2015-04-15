962ea093387c81e3acc696dd354902f0dfc0735a
Jalen Green <jalen.g@gmail.com>
2015-04-15 00:18:56 -0700

    Fix compile error

--- ps2/cs179_set2/transpose_cuda.cu
+++ ps2/cs179_set2/transpose_cuda.cu
@@ -75,7 +75,7 @@ void shmemTransposeKernel(const float *input, float *output, int n) {
 
   int beg_block = n * 64 * blockIdx.x + 64 * blockIdx.x;
 
-  for (c = 0; c < 4; c++) {
+  for (int c = 0; c < 4; c++) {
     output[beg_block + n * (threadIdx.y + c)] = data[idx];
     idx += 65;
   }
