667232e3f8defd85d48d77d1d62485c046025523
Jalen Green <jalen.g@gmail.com>
2015-04-15 13:06:48 -0700

    Comment, clean code

--- ps2/cs179_set2/transpose_cuda.cu
+++ ps2/cs179_set2/transpose_cuda.cu
@@ -3,18 +3,6 @@
 #include "transpose_cuda.cuh"
 
 /**
- * TODO for all kernels (including naive):
- * Leave a comment above all non-coalesced memory accesses and bank conflicts.
- * Make it clear if the suboptimal access is a read or write. If an access is
- * non-coalesced, specify how many cache lines it touches, and if an access
- * causes bank conflicts, say if its a 2-way bank conflict, 4-way bank
- * conflict, etc.
- *
- * Comment all of your kernels.
-*/
-
-
-/**
  * Each block of the naive transpose handles a 64x64 block of the input matrix,
  * with each thread of the block handling a 1x4 section and each warp handling
  * a 32x4 section.
@@ -27,22 +15,19 @@
  * This kernel is launched with block shape (64, 16) and grid shape
  * (n / 64, n / 64) where n is the size of the square matrix.
  *
- * You may notice that we suggested in lecture that threads should be able to
- * handle an arbitrary number of elements and that this kernel handles exactly
- * 4 elements per thread. This is OK here because to overwhelm this kernel
+ * This kernel handles exactly 4 elements per thread. 
+ * This is OK here because to overwhelm this kernel
  * it would take a 4194304 x 4194304  matrix, which would take ~17.6TB of
  * memory (well beyond what I expect GPUs to have in the next few years).
  */
 __global__
 void naiveTransposeKernel(const float *input, float *output, int n) {
-  // TODO: do not modify code, just comment on suboptimal accesses
 
-  const int i = threadIdx.x + 64 * blockIdx.x;
   int j = 4 * threadIdx.y + 64 * blockIdx.y;
   const int end_j = j + 4;
 
   for (; j < end_j; j++) {
-    // write output is non-coalesced, accesses are n floats apart,
+    // Write output is non-coalesced, accesses are n floats apart,
     // so each will hit a new cache line. (32 cache lines touched).
     output[j + n * i] = input[i + n * j];
   }
@@ -50,31 +35,30 @@ void naiveTransposeKernel(const float *input, float *output, int n) {
 
 __global__
 void shmemTransposeKernel(const float *input, float *output, int n) {
-  // TODO: Modify transpose kernel to use shared memory. All global memory
-  // reads and writes should be coalesced. Minimize the number of shared
-  // memory bank conflicts (0 bank conflicts should be possible using
-  // padding). Again, comment on all sub-optimal accesses.
 
-  __shared__ float data[4160];
+  __shared__ float data[4160]; // shmem array, 65x64
+                               // padded to avoid bank conflicts
 
   const int i = threadIdx.x + 64 * blockIdx.x;
   int j = 4 * threadIdx.y + 64 * blockIdx.y;
   const int end_j = j + 4;
-  int dj = 4 * threadIdx.y;
-  int di = threadIdx.x;
+
+  int dj = 4 * threadIdx.y; // col idx within the 64x64 block
+  int di = threadIdx.x;     // row idx within the 64x64 block
 
   for (; j < end_j; j++, dj++) {
-    //output[j + n * i] = input[i + n * j];
+    // Read from input coalesced, write to data in the transposed location.
     data[dj + (65 * di)] = input[i + n * j];
   }
   __syncthreads();
 
   j = 4 * threadIdx.y + 64 * blockIdx.y;
   dj = 4 * threadIdx.y;
-  int j0 = 64 * blockIdx.y;
-  int i0 = 64 * blockIdx.x;
+  int j0 = 64 * blockIdx.y; // col offset of 64x64 block within the nxn array
+  int i0 = 64 * blockIdx.x; // row offset of 64x64 block within the nxn array
   for (; j < end_j; j++, dj++) {
-    //output[j + n * i] = input[i + n * j];
+    // The block we are reading is already transposed, just need to
+    // write it the transposed position of the block in the larger nxn array
     output[j0 + n * i0 + di + n * dj] = data[di + (65 * dj)];
   }
 
@@ -83,16 +67,17 @@ void shmemTransposeKernel(const float *input, float *output, int n) {
 
 __global__
 void optimalTransposeKernel(const float *input, float *output, int n) {
-  // TODO: This should be based off of your shmemTransposeKernel.
-  // Use any optimization tricks discussed so far to improve performance.
-  // Consider ILP and loop unrolling.
 
-  __shared__ float data[4160];
+  __shared__ float data[4160]; // shmem array, 65x64
+                               // padded to avoid bank conflicts
 
   const int i = threadIdx.x + 64 * blockIdx.x;
   int j = 4 * threadIdx.y + 64 * blockIdx.y;
-  int dj = 4 * threadIdx.y;
-  int di = threadIdx.x;
+  int dj = 4 * threadIdx.y; // col idx within the 64x64 block
+  int di = threadIdx.x;     // row idx within the 64x64 block
+
+  // Unrolled loop with separated index calculations
+  // Arrangement of instructions altered to increase ILP
 
   int i_idx1 = i + n * j;
   int ditimes65 = 65 * di;
@@ -102,6 +87,7 @@ void optimalTransposeKernel(const float *input, float *output, int n) {
   int i_idx3 = i_idx1 + n + n;
   int i_idx4 = i_idx1 + n * 3;
 
+  // Read input, coalesced
   float i1 = input[i_idx1];
   float i2 = input[i_idx2];
   float i3 = input[i_idx3];
@@ -111,6 +97,7 @@ void optimalTransposeKernel(const float *input, float *output, int n) {
   int d_idx3 = d_idx1 + 2;
   int d_idx4 = d_idx1 + 3;
 
+  // Write to shmem in transposed position, no bank conflicts
   data[d_idx1] = i1;
   data[d_idx2] = i2;
   data[d_idx3] = i3;
@@ -133,11 +120,13 @@ void optimalTransposeKernel(const float *input, float *output, int n) {
 
   __syncthreads();
 
+  // Read from shmem, no bank conflicts
   float d1 = data[d_idx1];
   float d2 = data[d_idx2];
   float d3 = data[d_idx3];
   float d4 = data[d_idx4];
 
+  // Write to output, coalesced
   output[o_idx1] = d1;
   output[o_idx2] = d2;
   output[o_idx3] = d3;
