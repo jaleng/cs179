e165febdf1528071f46703ac1cfe72d268db980c
Jalen Green <jalen.g@gmail.com>
2015-05-11 19:26:48 -0700

    Change sloppyClusterKernel

--- ps5/cs179_set5/cluster_cuda.cu
+++ ps5/cs179_set5/cluster_cuda.cu
@@ -21,6 +21,7 @@ float atomicUpdateAverage(float* address, int n, float val)
   return __int_as_float(old);
 }
 
+
 // computes the distance squared between vectors a and b where vectors have
 // length size and stride stride.
 __device__ 
@@ -71,29 +72,30 @@ void sloppyClusterKernel(float *clusters, int *cluster_counts, int k,
 
   i = blockIdx.x * blockDim.x + threadIdx.x;
 
+  while (i < batch_size) {
+    float min_distance = squared_distance(smem, data, 1, REVIEW_DIM);
+    int closest_cluster = 0;
 
-  // TODO(jg) Figure out where to store this
-  float min_distance = squared_distance(smem, data, 1, REVIEW_DIM);
-  int closest_cluster = 0;
+    float *data_pt_start = data + REVIEW_DIM * i;
 
-  float *data_pt_start = data + REVIEW_DIM * i;
-
-  for (int j = 1; j < k; j++) {
-    // store the distance between review i and cluster j
-    float d = squared_distance(smem + REVIEW_DIM * j, data_pt_start, 1, REVIEW_DIM);
-    if (d < min_distance) {
-      min_distance = d;
-      closest_cluster = j;
+    for (int j = 1; j < k; j++) {
+      // store the distance between review i and cluster j
+      float d = squared_distance(smem + REVIEW_DIM * j, data_pt_start, 1, REVIEW_DIM);
+      if (d < min_distance) {
+        min_distance = d;
+        closest_cluster = j;
+      }
     }
-  }
 
-  output[i] = closest_cluster;
-  for (int feature = 0; feature < REVIEW_DIM; feature++) {
-    atomicUpdateAverage(clusters + REVIEW_DIM * closest_cluster, 
-                        cluster_counts[closest_cluster], 
-                        data_pt_start[feature]);
+    output[i] = closest_cluster;
+    for (int feature = 0; feature < REVIEW_DIM; feature++) {
+      int n = atomicAdd(cluster_counts[closest_cluster], 1);
+      atomicUpdateAverage(clusters + REVIEW_DIM * closest_cluster, 
+                          n, 
+                          data_pt_start[feature]);
+    }
+    i += gridDim.x * blockDim.x;
   }
-  cluster_counts[closest_cluster] += 1;
 }
 
 
